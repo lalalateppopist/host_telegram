62b3507e94a1d59f5fecdaa75feab673690a36b1
lalalateppopist <168966072+lalalateppopist@users.noreply.github.com>
2024-05-05 13:44:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from telegram.ext import Application, MessageHandler, filters
+
+BOT_TOKEN = "ваш токен тг бота"
+
+
+async def echo(update, context):
+    await update.message.reply_text(f'Я получил сообщение «{update.message.text}»')
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+    text_handler = MessageHandler(filters.TEXT, echo)
+    application.add_handler(text_handler)
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
